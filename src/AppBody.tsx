27da26b3368685114783386ccb48d6a40929cce7
Joseph Osorio <74214628+osorvr@users.noreply.github.com>
2021-11-22 16:40:43 -0800

    Add files via upload

---
+++ src/AppBody.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Card } from '../uikit'
+
+interface interfaceMax {
+  max?: boolean
+  mt?: string
+}
+
+export const BodyWrapper = styled(Card)<interfaceMax>`
+  position: relative;
+  max-width: ${(props) => (props.max ? '1200px' : '436px')};
+  width: 100%;
+  z-index: 5;
+  margin-top: ${props => props.mt ? props.mt : 0};
+`
+
+/**
+ * The styled container element that wraps the content of most pages and the tabs.
+ */
+export default function AppBody({ children, max = false, mt = '' }: { children: React.ReactNode; max?: boolean, mt?: string }) {
+  return <BodyWrapper max={max} mt={mt}>{children}</BodyWrapper>
+}
