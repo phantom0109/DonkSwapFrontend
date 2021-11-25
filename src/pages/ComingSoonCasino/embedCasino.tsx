aa9dc825e7054ef612b0bc1ceb9d56d46c0e990a
DendiBot <sujithisathome@gmail.com>
2021-11-25 05:35:59 +0530

    fixed video issues

--- src/pages/ComingSoonCasino/embedCasino.tsx
+++ src/pages/ComingSoonCasino/embedCasino.tsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 const embedCasino = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
+      className="vid-iframe"
       src={`https://www.youtube.com/embed/${embedId}?autoplay=1&vq=hd720`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
