f62d37da40e66fc3f7eafb12086a1b58d0376d5d
Jimmy Stax <87676344+JimmyStax90@users.noreply.github.com>
2021-12-09 16:26:17 -0800

    Update index.tsx

--- src/pages/ComingSoonCasino/index.tsx
+++ src/pages/ComingSoonCasino/index.tsx
@@ -7,4 +7,16 @@ export default function Casino() {
       <EmbedCasino embedId="h-vkM3ONxTA" />
     </div>
   );
+  (
+  return (
+    <div className="vid-container">
+      <EmbedCasino embedId="GaUw4aCw3QU" />
+    </div>
+  );
+  (
+  return (
+    <div className="vid-container">
+      <EmbedCasino embedId="nam7L4Z-hUc" />
+    </div>
+  );
 }
