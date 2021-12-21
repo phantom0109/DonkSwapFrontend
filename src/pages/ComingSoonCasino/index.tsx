50772dbc2d522003fc65003dafeab83d25beb07a
radenlmantuano <newcodeondablock@gmail.com>
2021-12-20 23:24:17 -0800

    style(HomePage UI): Raden Homepage UI Revision 3

--- src/pages/ComingSoonCasino/index.tsx
+++ src/pages/ComingSoonCasino/index.tsx
@@ -1,6 +1,7 @@
 import "./comingsooncasino.css";
 import casinoGameOne from './video/Casino_Slots1.mp4'
 import casinoGameThree from './video/Casino _Slots3.mp4'
+import comingSoonVid from './video/Casino_ComingSoon.mp4'
 
 
 
@@ -11,6 +12,14 @@ const Casino = (props) => {
   return (
     <div>
       <div className="vid-container">
+
+      <div className="youtube-hugger">
+          <video loop autoPlay muted className="casino-vid-tab"> 
+            <source src={(comingSoonVid)} type="video/mp4" />
+          </video>
+          <a href="https://youtu.be/h-vkM3ONxTA" target="_blank">See it on youtube..</a>
+        </div>
+
         <div className="youtube-hugger">
           <video loop autoPlay muted className="casino-vid-tab"> 
             <source src={(casinoGameOne)} type="video/mp4" />
