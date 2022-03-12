e8045e3c26d7b67d259bc378683d4b7710a9e5af
Jimmy Stax <87676344+JimmyStax90@users.noreply.github.com>
2022-03-11 17:53:18 -0800

    Update NCAATournament.tsx

--- src/pages/MarchMadness/NCAATournament.tsx
+++ src/pages/MarchMadness/NCAATournament.tsx
@@ -10,7 +10,7 @@ const NCAATournament = (props) => {
         <div id='mainbody-container'>
             <section className='section-one'>
                 <div className='head-line-section'>
-                    <h1>Rebecca Grant's March Mania Tournament </h1>
+                    <h1>Rebecca Grant's Bracket Challenge</h1>
                 </div>
                     <img src={bannerImg}/>
             </section>
@@ -21,11 +21,17 @@ const NCAATournament = (props) => {
                 <div id='rules' className='content'>
                     <ul>
                         <li>Rule 1</li>
-                        <p>This is just a placeholder text</p>
+                        <p>$100 Per Entry - Paid in BUSD, BNB or ETH</p>
                         <li>Rule 2</li>
-                        <p>This is just a placeholder text</p>
+                        <p>Funds must be sent by 6am PST on 3/17 to be eligible.</p>
                         <li>Rule 3</li>
-                        <p>This is just a placeholder text</p>
+                        <p>Entry fee must be sent when bracket is submitted</p>
+                        <li>Rule 4</li>
+                        <p>Google Form Registration is Mandatory</p>
+                        <li>Rule 5</li>
+                        <p>Second Round - Rebuy $75 Per Entry</p>
+                        <li>Rule 6</li>
+                        <p>Third Round & Beyond - Rebuy $50 Per Entry</p>
                     
                     </ul>
                 </div>
