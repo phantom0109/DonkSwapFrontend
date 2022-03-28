8644aab01d9b858a8b28e339fc517e3964410e17
bluedragon1120 <bluedragon1120@163.com>
2022-03-28 13:55:25 +0800

    removed Binance Chain Wallet, MathWallet, TokenPocket

--- src/uikit/widgets/WalletModal/config.tsx
+++ src/uikit/widgets/WalletModal/config.tsx
@@ -1,11 +1,7 @@
 import Metamask from "./icons/Metamask";
-import MathWallet from "./icons/MathWallet";
-import TokenPocket from "./icons/TokenPocket";
 import TrustWallet from "./icons/TrustWallet";
 import WalletConnect from "./icons/WalletConnect";
-import BinanceChain from "./icons/BinanceChain";
 import SafePalWallet from "./icons/SafePalWallet";
-// import OntoWallet from './icons/OntoWallet';
 import { Config, ConnectorNames } from "./types";
 
 const connectors: Config[] = [
@@ -22,26 +18,6 @@ const connectors: Config[] = [
   {
     title: "TrustWallet",
     icon: TrustWallet,
-    connectorId: ConnectorNames.Injected,
-  },
-  {
-    title: "Binance Chain Wallet",
-    icon: BinanceChain,
-    connectorId: ConnectorNames.BSC,
-  },
-  // {
-  //   title: "ONTO Wallet",
-  //   icon: OntoWallet,
-  //   connectorId: ConnectorNames.Injected,
-  // },
-  {
-    title: "MathWallet",
-    icon: MathWallet,
-    connectorId: ConnectorNames.Injected,
-  },
-  {
-    title: "TokenPocket",
-    icon: TokenPocket,
     connectorId: ConnectorNames.Injected,
   },
   {
