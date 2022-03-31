a1e139e41ad16e53d45dad125b0d26e1639c798e
radenlmantuano <newcodeondablock@gmail.com>
2022-03-30 20:08:37 -0700

    style(website ui): Add Headline Image and ui

--- src/pages/ComingSoonCasino/index.tsx
+++ src/pages/ComingSoonCasino/index.tsx
@@ -1,25 +1,19 @@
 import "./comingsooncasino.css";
 import casinoGameOne from './video/Casino_Slots1.mp4'
 import casinoGameThree from './video/Casino _Slots3.mp4'
-import comingSoonVid from './video/Casino_ComingSoon.mp4'
+import headerPhoto from './images/photo_2022-03-30 19.48.13.jpeg'
 
 
 
 
 const Casino = (props) => {
 
-
   return (
     <div>
-        <h1 className="coming-soon-title">COMING SOON!</h1>
+      <div className="headerPhoto-container">
+        <img className="headerPhoto" src={headerPhoto} alt="" />
+      </div>
       <div className="vid-container">
-      <div className="youtube-hugger">
-          <video controls controlsList="nodownload" muted disablePictureInPicture className="casino-vid-tab"> 
-            <source src={(comingSoonVid)} type="video/mp4" />
-          </video>
-          <a href="https://youtu.be/h-vkM3ONxTA" target="_blank">See it on youtube..</a>
-        </div>
-
         <div className="youtube-hugger">
           <video controls controlsList="nodownload" muted disablePictureInPicture className="casino-vid-tab"> 
             <source src={(casinoGameOne)} type="video/mp4" />
